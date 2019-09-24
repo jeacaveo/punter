8af04ae267322912c015e17d6f3bc531b648c109
Jean Ventura <jv@venturasystems.net>
2019-09-24 08:54:59 -0700

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="punter_jeacaveo",
+    version="1.0.0",
+    author="Jean Ventura",
+    author_email="jv@venturasystems.net",
+    description="Data fetcher for the Prismata strategy game",
+    long_description=long_description,
+    long_description_content_type="text/x-rst; charset=UTF-8",
+    url="https://github.com/jeacaveo/punter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: "
+        "GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        ],
+    python_requires=">=3.6",
+    install_requires=["beautifulsoup4", "requests"],
+    extras_require={
+        "test": ["mock", "coverage"],
+        },
+    )
