887b0220d21dc77495e973cd29c0156e85dbdc00
Jean Ventura <jv@venturasystems.net>
2019-09-30 07:36:31 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
 with open("README.rst", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
     name="punter_jeacaveo",
-    version="1.0.0",
+    version="1.0.1",
     author="Jean Ventura",
     author_email="jv@venturasystems.net",
     description="Data fetcher for the Prismata strategy game",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/x-rst; charset=UTF-8",
     url="https://github.com/jeacaveo/punter",
     packages=setuptools.find_packages(),
@@ -23,6 +23,7 @@ setuptools.setup(
     python_requires=">=3.6",
     install_requires=["beautifulsoup4", "requests"],
     extras_require={
+        "dev": ["pycodestyle", "pylint", "mypy"],
         "test": ["mock", "coverage"],
         },
     )
