893cb6448aab3909a008d2691296186bda2a8823
Jean Ventura <jv@venturasystems.net>
2019-12-17 08:53:24 -0800

    Add loggging config

--- punter/scrape/config.py
+++ punter/scrape/config.py
@@ -1,4 +1,9 @@
 """ Configuration for all things scrape. """
+from logging import (
+    config as loggingConfig,
+    getLogger,
+    )
+from os import path
 
 
 GENERAL = {
@@ -6,8 +11,14 @@ GENERAL = {
     }
 
 PRISMATA_WIKI = {
-    # Change BASE_URL to a path to a directory to avoid making http requests
-    "BASE_URL": "https://prismata.gamepedia.com",
+    # Change BASE_URL to a URL to scrape from a site.
+    "BASE_URL": "punter/scrape/files/wiki",  # "https://prismata.gamepedia.com"
     "UNITS_PATH": "/Unit",
     "SAVE_PATH": "punter/scrape/files/wiki",
     }
+
+CURRENT_DIR = path.dirname(path.abspath(__file__))
+loggingConfig.fileConfig(
+    path.join(CURRENT_DIR, "../logging.conf"),
+    defaults={"logfilename": path.join(CURRENT_DIR, "../../log")})
+LOGGER = getLogger("scrape")
